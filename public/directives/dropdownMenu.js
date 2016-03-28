df92275dd91282235ca612d3c10bf3c43cbb6c09
asBrettisay <brett.caudill.is@gmail.com>
2016-03-28 15:22:36 -0600

    Improved dropdown menu.

--- public/directives/dropdownMenu.js
+++ public/directives/dropdownMenu.js
@@ -22,6 +22,7 @@ angular.module('influences')
         $scope.menuActive = false;
       })
 
+
     },
     link: function(scope, elem, attrs) {
       var menuButton = elem.find('div.menuButtonContainer');
